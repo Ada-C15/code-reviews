0b4a2c21b9d36a3d2330c21779258b1593912089
CheezItMan <chris@adadevelopersacademy.org>
2019-08-08 15:17:49 -0700

    is all unique elements function

---
+++ is_all_unique_elements.js
@@ -0,0 +1,14 @@
+
+const isAllUniqueElements = (array) => {
+  let isAllUnique = true;
+
+  array.forEach((currentElement, currentIndex) => {
+    array.slice(currentIndex + 1).forEach((elementToCompare) => {
+      if (elementToCompare === currentElement) {
+        isAllUnique = false;
+      }
+    });
+  });
+
+  return isAllUnique;
+};
